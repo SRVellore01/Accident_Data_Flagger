f96dbcbbf15413cd859a72b6cc42ceafc155cbeb
SRVellore01 <99672226+SRVellore01@users.noreply.github.com>
2022-03-25 14:27:47 -0400

    Add files via upload
    Added Files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccidentDataFlagger
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //Service1 service = new Service1();
+            //if (Environment.UserInteractive)
+            //{
+            //    service.RunAsConsole();
+            //}
+
+            log4net.Config.XmlConfigurator.Configure();
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
